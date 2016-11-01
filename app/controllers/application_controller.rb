abbb52b2c3192d08b1aa3fca06cffe01877ef420
Melissa Lau <mlau@Melissas-MacBook-Pro.local>
2016-11-01 10:58:36 -0700

    working backend auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def logout
     current_user.reset_session_token
-    @session[:session_token] = nil
+    session[:session_token] = nil
   end
 
   def logged_in?
