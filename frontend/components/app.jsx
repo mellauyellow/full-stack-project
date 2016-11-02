66b2d4e98fcaae85785ea48ad2daa907b7fc5ac2
Melissa Lau <mlau@Melissas-MacBook-Pro.local>
2016-11-01 18:21:51 -0700

    basic styling on header

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,11 +3,13 @@ import AuthContainer from './header/auth/auth_container';
 
 const App = ({children}) => (
   <div className="app">
-    <div className="logo-and-name">
-      <img src="http://www.culhanemeadows.com/wp-content/uploads/2015/11/emoji.png" className="logo"></img>
-      <h1 className="company-name">onward.</h1>
-    </div>
-    <AuthContainer />
+    <nav>
+      <div className="logo-and-name">
+        <img src="http://www.culhanemeadows.com/wp-content/uploads/2015/11/emoji.png" className="logo"></img>
+        <h1 className="company-name">onward</h1>
+      </div>
+      <AuthContainer />
+    </nav>
     {children}
   </div>
 );
